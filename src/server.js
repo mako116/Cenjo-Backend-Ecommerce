544ad862b4efb97d422dc6141d271a1e8a390f70
emmanuel <nwakwueemmanuel1@gmail.com>
2026-04-09 21:54:55 +0100

    fixed issue

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ if (ENV.NODE_ENV === "production") {
 const startServer = async () => {
   await connectDB();
   app.listen(ENV.PORT, () => {
-    console.log("Server is up and running");
+    console.log("Server is running");
   });
 };
 
