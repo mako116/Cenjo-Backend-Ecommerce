a8f10921c7c8e5f0469041b2625d15582c9f7fae
emmanuel <nwakwueemmanuel1@gmail.com>
2026-04-09 23:36:00 +0100

    removed console logs

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from "cloudinary";
-
 import { ENV } from "./env.js";
 
 cloudinary.config({
--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,7 @@ import { ENV } from "./env.js";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(ENV.DB_URL);
-    console.log(` Connected to MONGODB: ${conn.connection.host}`);
   } catch (error) {
-    console.error(" MONGODB connection error");
     process.exit(1);
   }
 };
