0b3e1d5aa8a3cd19b6e77c82433e4eaf443a0427
emmanuel <nwakwueemmanuel1@gmail.com>
2026-04-10 15:27:44 +0100

    uncommited the cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
