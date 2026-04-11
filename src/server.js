cb3c5422a701f77febc2854a8cfe0ded54d0d7c8
emmanuel <nwakwueemmanuel1@gmail.com>
2026-04-11 15:56:13 +0100

    fixed issues

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import cors from "cors";
 import { ENV } from "./config/env.js";
+import adminRoutes from "./routes/admin.route.js";
 
 import { connectDB } from "./config/db.js";
 import { inngest, functions } from "./config/inngest.js";
@@ -15,6 +16,8 @@ app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/admin", adminRoutes);
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
