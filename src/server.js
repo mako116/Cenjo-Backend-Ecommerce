dc26528d1a82df085643ea42b8506403315fcdc3
emmanuel <nwakwueemmanuel1@gmail.com>
2026-04-13 18:08:46 +0100

    feat: initialize express server with clerk authentication, inngest integration, and production static file serving

--- src/server.js
+++ src/server.js
@@ -13,11 +13,11 @@ import adminRoutes from "./routes/admin.route.js";
 import { connectDB } from "./config/db.js";
 import { inngest, functions } from "./config/inngest.js";
 import userRoutes from "./routes/user.route.js";
-import orderRoutes from "./routes/order.route.js";
-import reviewRoutes from "./routes/review.route.js";
-import productRoutes from "./routes/product.route.js";
-import cartRoutes from "./routes/cart.route.js";
-import paymentRoutes from "./routes/payment.route.js";
+// import orderRoutes from "./routes/order.route.js";
+// import reviewRoutes from "./routes/review.route.js";
+// import productRoutes from "./routes/product.route.js";
+// import cartRoutes from "./routes/cart.route.js";
+// import paymentRoutes from "./routes/payment.route.js";
 
 const app = express();
 
