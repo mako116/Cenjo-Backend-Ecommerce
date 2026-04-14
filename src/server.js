1b6863537c88ad5093cb5e70850ee2ff435fbb79
emmanuel <nwakwueemmanuel1@gmail.com>
2026-04-14 22:53:52 +0100

    feat: implement product database seeding script and add npm command for execution

--- src/server.js
+++ src/server.js
@@ -36,12 +36,29 @@ app.use(
 );
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "http://localhost:8080", 
+  ENV.CLIENT_URL
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
-
+//  npm run seed:products
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
